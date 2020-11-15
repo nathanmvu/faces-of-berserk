2b6b5b061d5f9cf71db48ca3c3937b9a7f6530fe
Nathan Vu <nathanvu99@gmail.com>
2020-11-14 22:58:33 -0800

    Added more characters

--- members-only/src/components/Header/Header.js
+++ members-only/src/components/Header/Header.js
@@ -2,10 +2,10 @@ import './Header.css';
 
 function Header() {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">The Characters of Berserk</h1>
-        <p class="lead">Search and view the details of the characters of the popular manga, Berserk!</p>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">The Characters of Berserk</h1>
+        <p className="lead">Search and view the details of the characters of the popular manga, Berserk!</p>
       </div>
     </div>
   )
