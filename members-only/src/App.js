bf0a0252d122bf64a6f0436d0c04d34b4f8a66ca
Nathan Vu <nathanvu99@gmail.com>
2020-11-14 22:23:15 -0800

    Added images can sort alphabetically

--- members-only/src/App.js
+++ members-only/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
-// import List from './components/List/List';
-import Characters from './components/members.json';
+import List from './components/List/List';
+import Characters from './components/Characters.json';
 import './App.css';
 
 function App() {
+  const [characterState, setCharacters] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case 'change':
+          return action.input;
+        case 'sort':
+          return [...state].sort((a, b) => (a.name > b.name) ? 1 : -1);
+        default:
+          return Characters;
+      }
+    }, Characters)
 
   const handleInputChange = (search) => {
     const searchArray = [];
@@ -22,6 +33,7 @@ function App() {
       <Search 
         handleInputChange = { handleInputChange }
       />
+      <List characters={ characterState } setCharacters={ setCharacters }/>
     </div>
   );
 }
--- members-only/src/components/List/List.js
+++ members-only/src/components/List/List.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import './List.css';
+
 function List({ characters, setCharacters}) {
   return (
     <div className='container'>
@@ -6,15 +9,21 @@ function List({ characters, setCharacters}) {
           <th scope='col'>Image</th>
           <th scope='col' onClick={() => setCharacters({ type: 'sort'})}>Name</th>
           <th scope='col'>Character Type</th>
+          <th scope='col'>Weapon</th>
         </thead>
         <tbody>
           {characters.map(character => 
           <tr key={character.id}>
             <td><img id={character.name} src={character.image} alt={character.name} /></td>
+            <td>{character.name}</td>
+            <td>{character.type}</td>
+            <td>{character.weapon}</td>
           </tr>
-          }
+          )}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
+
+export default List;
